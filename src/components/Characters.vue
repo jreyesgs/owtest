5bbbaa07f7b20b0dd022cfcb5e09bc3883e1ba25
Guillermo <you@example.com>
2021-11-05 16:49:12 +0100

    update

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -1,25 +1,65 @@
 <template>
-<div class="row">
+  <div class="row">
     <div class="col">
-        <h2>List of Character</h2>
+      <h2>List of Character</h2>
     </div>
-</div>
-<!-- Utiliza componentes siempre que sea necesarios para mejorar la usabilidad del código -->
-<nav class="navbar">
-    <!-- 
-            TODO: Añadir aquí una barra de navegación que permita realizar busqueda por nombre y filtra sobre los resultados por:
-            
-            name: Buscar por el dado name.
-            status: filtrar por el dado status (alive, dead o unknown).
-            species: filtrar por el dado species (Human, Alien).
-            gender: filtrar por el dado gender (female, male, genderless o unknown).
-
-            DOC: https://rickandmortyapi.com/documentation/#rest
-    -->
-</nav>
-<div class="row mt-3">
+  </div>
+  <!-- Utiliza componentes siempre que sea necesarios para mejorar la usabilidad del código -->
+  <nav class="navbar navbar-expand-lg">
+ <div class="container-fluid">
+    <a class="navbar-brand item" @click.prevent="filterStatus('')">All</a>
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+       
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Status
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item item" @click.prevent="filterStatus('Alive')" >Alive</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item item" @click.prevent="filterStatus('Dead')">Dead</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item item" @click.prevent="filterStatus('unknown')">Unknown</a></li>
+          </ul>
+        </li>
+       <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Species
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item item" @click.prevent="filterSpecies('Human')">Human</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item item" @click.prevent="filterSpecies('Alien')">Alien</a></li>
+           
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Gender
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item item" @click="filterGender('Male')">Male</a></li>
+            <li><hr class="dropdown-divider item"></li>
+            <li><a class="dropdown-item item" @click="filterGender('Female')">Female</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item item" @click="filterGender('Genderless')">Genderless</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item item" @click="filterGender('Unknown')">Unknown</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" v-model="name" type="search" placeholder="Search by name" aria-label="Search">
+        <button class="btn btn-outline-success" @click.prevent="getCharacter()" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+  </nav>
+  <div class="row mt-3">
     <div v-for="character in characters" :key="character.id" class="col-lg-4">
-        <!--
+      <!--
             TODO: Añadir los datos de las fichas de los personajes para completarla:
 
             - Nombre
@@ -34,76 +74,251 @@
             Usando como base esta card de Bootstrap 5, modificala para adaptarla a las necesidades de los datos.
 
         -->
-        <div class="card mb-3">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="" class="img-fluid rounded-start" alt="">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        <!-- TODO: Añade un botón que abra el detalle del personaje a través de un modal-->
-                    </div>
-                </div>
+      <div class="card mb-3" data-bs-toggle="modal" data-bs-target="#card"  @click="showModal(character)">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img
+              :src="character.image"
+              class="img-fluid rounded-start"
+              alt=""
+            />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">{{ character.name }}</h5>
+              <p class="card-text">
+                {{ character.status }} - {{ character.species }} -
+                {{ character.gender }}
+              </p>
+              <p class="card-text">
+                <small class="text-muted">{{ character.origin.name }}</small>
+              </p>
+            
+              <!-- TODO: Añade un botón que abra el detalle del personaje a través de un modal-->
+               
             </div>
+          </div>
         </div>
+      </div>
+          
     </div>
-</div>
+  
+  </div>
+  
+  <div class="modal fade" id="card" tabindex="-1" aria-labelledby="cardInfo" >
+  <div class="modal-dialog">
+   
+     
+<div class="modal-body"> 
+<CaracterDetail v-if="isModalVisible"  :character='characterInfo'/> 
+    
 
-<footer>
-    <!--
-        TODO: Como extra sería ideal que añadieras un paginador con el total de páginas y la opción de navegar entre ellas << n >> 
-    -->
-</footer>
+</div>
+</div>
+</div>
+  <footer>
+   	<!-- <nav class="app-pagination">
+							<ul class="pagination justify-content-center">
+								<li class="page-item ">
+									<a class="page-link" href="#" v-if="pages.current > 1" @click.prevent="changePage(pages.current - 1)">Anterior</a>
+							    </li>
+								
+								<li class="page-item " exact-active-class="active" v-for="page in pagesNumber" :key="page.to"  @click.prevent="changePage(page)"><a class="page-link" href="#"  >{{page}}</a></li>
+								
+								<li class="page-item">
+								    <a class="page-link" href="#" v-if="pages.current <  pages.last_page" @click.prevent="changePage(pages.current + 1)">Siguiente</a>
+								</li>
+							</ul>
+						</nav>//app-pagination -->
+  </footer>
+
 </template>
 
 <script>
+import axios from "axios";
+import CaracterDetail from '@/components/CaracterDetail.vue'
 export default {
-    name: 'Character',
-    data(){
-        return {
-            characters: [
-                {
-                    "id": 1,
-                    "name": "Rick Sanchez",
-                    "status": "Alive",
-                    "species": "Human",
-                    "type": "",
-                    "gender": "Male",
-                    "origin": {
-                        "name": "Earth (C-137)",
-                        "url": "https://rickandmortyapi.com/api/location/1"
-                    },
-                    "location": {
-                        "name": "Earth (Replacement Dimension)",
-                        "url": "https://rickandmortyapi.com/api/location/20"
-                    },
-                    "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-                    "episode": ["https://rickandmortyapi.com/api/episode/1", "https://rickandmortyapi.com/api/episode/2", "https://rickandmortyapi.com/api/episode/3", "https://rickandmortyapi.com/api/episode/4", "https://rickandmortyapi.com/api/episode/5", "https://rickandmortyapi.com/api/episode/6", "https://rickandmortyapi.com/api/episode/7", "https://rickandmortyapi.com/api/episode/8", "https://rickandmortyapi.com/api/episode/9", "https://rickandmortyapi.com/api/episode/10", "https://rickandmortyapi.com/api/episode/11", "https://rickandmortyapi.com/api/episode/12", "https://rickandmortyapi.com/api/episode/13", "https://rickandmortyapi.com/api/episode/14", "https://rickandmortyapi.com/api/episode/15", "https://rickandmortyapi.com/api/episode/16", "https://rickandmortyapi.com/api/episode/17", "https://rickandmortyapi.com/api/episode/18", "https://rickandmortyapi.com/api/episode/19", "https://rickandmortyapi.com/api/episode/20", "https://rickandmortyapi.com/api/episode/21", "https://rickandmortyapi.com/api/episode/22", "https://rickandmortyapi.com/api/episode/23", "https://rickandmortyapi.com/api/episode/24", "https://rickandmortyapi.com/api/episode/25", "https://rickandmortyapi.com/api/episode/26", "https://rickandmortyapi.com/api/episode/27", "https://rickandmortyapi.com/api/episode/28", "https://rickandmortyapi.com/api/episode/29", "https://rickandmortyapi.com/api/episode/30", "https://rickandmortyapi.com/api/episode/31", "https://rickandmortyapi.com/api/episode/32", "https://rickandmortyapi.com/api/episode/33", "https://rickandmortyapi.com/api/episode/34", "https://rickandmortyapi.com/api/episode/35", "https://rickandmortyapi.com/api/episode/36", "https://rickandmortyapi.com/api/episode/37", "https://rickandmortyapi.com/api/episode/38", "https://rickandmortyapi.com/api/episode/39", "https://rickandmortyapi.com/api/episode/40", "https://rickandmortyapi.com/api/episode/41"],
-                    "url": "https://rickandmortyapi.com/api/character/1",
-                    "created": "2017-11-04T18:48:46.250Z"
-                },
-                // ...
-            ]
-        }
-    },
-    methods:{
-        getCaraters(){
-            /* 
-                TODO: Añadir la lógica necesaria para cargar los datos de los personajes de Rick and Morty atacando el endpoint
-                https://rickandmortyapi.com/api/character y usando Axios*
+  name: "Character",
+  components: {CaracterDetail},
+  data() {
+    return {
+      characters: [],
+      name: "",
+      pages:'',
+      isModalVisible: false,
+      characterInfo:{}
+    };
+  },
+  created(){
+     this.getCharacters()
 
-                * Axios ya está instalado y configurado
-            */
-        }
+
+
+  },
+
+  methods: {
+    getCharacters(){
+         axios.get('https://rickandmortyapi.com/api/character') 
+         .then((res)=>{
+             for (let i = 0; i < res.data.results.length; i++) {
+            this.characters.push(res.data.results[i]);
+          }
+         })
+         .catch((error)=>{
+           console.log(error)
+         })
+      },
+    getCharacter() {
+      this.characters = [];
+      const formatName = this.name.toLowerCase();
+      axios
+        .get("https://rickandmortyapi.com/api/character/?name=" + formatName)
+        .then((res) => {
+          for (let i = 0; i < res.data.results.length; i++) {
+            this.characters.push(res.data.results[i]);
+          }
+        })
+         .catch((error)=>{
+           console.log(error)
+         });
     },
-}
+    filterStatus(status) {
+      this.characters = [];
+      const formatName = this.name.toLowerCase();
+      axios
+        .get(
+          "https://rickandmortyapi.com/api/character/?name=" +
+            formatName +
+            "&status=" +
+            status
+        )
+        .then((res) => {
+          
+          for (let i = 0; i < res.data.results.length; i++) {
+            this.characters.push(res.data.results[i]);
+          }
+        })
+         .catch((error)=>{
+           console.log(error)
+         });
+    },
+    filterSpecies(specie) {
+      this.characters = [];
+      const formatName = this.name.toLowerCase();
+      axios
+        .get(
+          "https://rickandmortyapi.com/api/character/?name=" +
+            formatName +
+            "&species=" +
+            specie
+        )
+        .then((res) => {
+          
+          for (let i = 0; i < res.data.results.length; i++) {
+            this.characters.push(res.data.results[i]);
+          }
+        })
+         .catch((error)=>{
+           console.log(error)
+         });
+    },
+    filterGender(gender) {
+      this.characters = [];
+      const formatName = this.name.toLowerCase();
+      axios
+        .get(
+          "https://rickandmortyapi.com/api/character/?name=" +
+            formatName +
+            "&gender=" +
+            gender
+        )
+        .then((res) => {
+         
+          for (let i = 0; i < res.data.results.length; i++) {
+            this.characters.push(res.data.results[i]);
+          }
+        })
+         .catch((error)=>{
+           console.log(error)
+         });
+    },
+    showModal(data) {
+      this.characterInfo = data;
+       this.isModalVisible = !this.isModalVisible;
+       
+      
+      
+      console.log(this.characterInfo)
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-    /*
-        TODO: Añade los estilos que consideres necesarios para que la card quere correctamente.
-     */
+button{
+    border: 2px solid white!important;
+    color: var(--text-white)!important
+}
+.mt-3{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 3rem;
+    margin: 3rem 0;
+}
+.card{
+  background-color: var(--background-card)!important;
+  border-radius: 20px;
+  box-shadow: 0 0 10px 1px var(--background-body);
+  overflow: hidden;
+  cursor: pointer;
+  transition: transform 200ms ease-in-out;
+  height: 100%;
+  &:hover{
+    transform: scale(1.05);
+    h5{
+      color: var(--text-orange);
+    }
+  }
+  span{
+    color: var(--text-gray)!important;
+  }
+  h5{
+    margin-bottom: 0.5rem!important;
+  }
+ 
+    .origen{
+      margin-bottom: 0.5rem;
+    }
+
+
+}
+  .search{
+    width: 400px;
+    margin: 3rem auto 0;
+    input{
+        height: 53px;
+        width: 400px;
+        border: none;
+        border-radius: 10px;
+        padding: 0 0.5rem;
+    }
+}
+.filter{
+    width: 400px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    border-radius: 10px;
+    overflow: hidden;
+    .item{
+        padding: 1rem 0.5rem;
+        background-color: var(--background-card);
+        text-align: center;
+        cursor: pointer;
+        &:hover{
+            color: var(--text-orange);
+        }
+        
+    }
+}
 </style>
--- src/components/Episodes.vue
+++ src/components/Episodes.vue
@@ -1,14 +1,69 @@
 <template>
-  <div>
+ <div class="row mt-3">
+  <div v-for="episode in episodes" :key="episode.id" class="col-lg-4">
       <!-- 
           TODO: Crea el listado de episodios con cards y añadir el detalle del episodios con la vista previa de los personajes.
       -->
+      <div class="card" style="width: 18rem;">
+  <div class="card-header">
+    {{episode.name}}
   </div>
+  <div>
+    {{episode.air_date}}
+  </div>
+  <div>
+    <button @click="getCharacter">View Characters</button>
+  </div>
+  <ul class="list-group list-group-flush" v-for="character in characters" :key="character.id" >
+    <li class="list-group-item"><img :src="character.image" alt="">
+    <span>{{characters.name}}</span></li>
+   
+  </ul>
+</div>
+  </div>
+ </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+  data(){
+    return{
+      episodes:[],
+      characters:[]
+    }
+    
+  },
+  created(){
+this.getEpisodes()
 
+  },
+methods:{
+  getEpisodes(){
+    axios.get('https://rickandmortyapi.com/api/episode')
+    .then((res)=>{
+       for (let i = 0; i < res.data.results.length; i++) {
+            this.episodes.push(res.data.results[i]);
+       }
+     
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+
+  },
+  getCharacter(){
+    for(let i = 0; i < this.episodes.characters.length; i++){
+          axios.get(''+this.episodes.characters[i]+'')
+            .then((res)=>{
+              this.characters.push(this.episodes.characters[i])
+            })
+            .catch((error)=>{
+              console.log(error)
+            })
+          }
+  }
+}
 }
 </script>
 
